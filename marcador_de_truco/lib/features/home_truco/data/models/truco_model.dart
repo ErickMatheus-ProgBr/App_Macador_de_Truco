784eccac60b0c02478a0c1853713d07c5ee7cbe6
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-07-04 10:54:31 -0300

    feat(truco):cria um modelo de dados TrucoModel

--- marcador_de_truco/lib/features/home_truco/data/models/truco_model.dart
+++ marcador_de_truco/lib/features/home_truco/data/models/truco_model.dart
@@ -1,11 +1,28 @@
-class userModel {
+class TrucoModel {
   final String gameName;
+  final String caption;
   final String timeA;
   final String timeB;
+  final int pointsA;
+  final int pointsB;
 
-  const userModel({required this.gameName, required this.timeA, required this.timeB});
+  TrucoModel({
+    required this.gameName,
+    required this.caption,
+    required this.timeA,
+    required this.timeB,
+    required this.pointsA,
+    required this.pointsB,
+  });
 
-  factory userModel.fromJson(Map<String, dynamic> json) {
-    return userModel(gameName: json["TRUCO"], timeA: json["Time A"], timeB: json["Time B"]);
+  factory TrucoModel.fromJson(Map<String, dynamic> json) {
+    return TrucoModel(
+      gameName: json["TRUCO"] ?? "Truco",
+      caption: json["Marcador"],
+      timeA: json["Time A"],
+      timeB: json["Time B"],
+      pointsA: json["Pontos Time A"] ?? 0,
+      pointsB: json["Pontos Time B"] ?? 0,
+    );
   }
 }
