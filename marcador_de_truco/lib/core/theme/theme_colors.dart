388a5b61dce2fd6fb8273a72b2bd50ac2f81fca8
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-07-04 11:29:26 -0300

    feat/style(thme): arquivo destinado para agrupar cores pre-definidos

---
+++ marcador_de_truco/lib/core/theme/theme_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  ThemeData myThemeApp = ThemeData(
+    colorScheme: ColorScheme.fromSwatch().copyWith(primary: Colors.red, secondary: Colors.black),
+    textTheme: const TextTheme(
+      // Agrupamentos de estilo de texto para o app.
+      bodyLarge: TextStyle(color: Colors.white), // estilo pré-definido
+      bodyMedium: TextStyle(color: Colors.white),
+      titleLarge: TextStyle(color: Colors.white),
+    ),
+  );
+}
