c0c54649c64e9f9bc5396c7adfd11292fe88c492
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-07-06 19:21:59 -0300

    style/docs(fonts): manutenção ok

--- marcador_de_truco/lib/core/fonts/customFonts.dart
+++ marcador_de_truco/lib/core/fonts/customFonts.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:google_fonts/google_fonts.dart'; // 1. Importe o pacote aqui
-import 'package:marcador_de_truco/core/theme/theme_colors.dart';
 
 class Customfonts {
+  // Nome do Aplicativo
+  static TextStyle nameApp = GoogleFonts.changa(fontSize: 24, fontWeight: FontWeight.bold);
+
+  // Sub titulo do app
+  static TextStyle subTitle = GoogleFonts.changa(fontSize: 11, fontWeight: FontWeight.w500);
+
   // Estilo para o campo de nome das equipes
   static TextStyle timeNameStyle = GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.bold);
 
@@ -12,4 +17,7 @@ class Customfonts {
 
   // Botões de decrementar e incrementar
   static TextStyle counter = GoogleFonts.bungee(fontSize: 14, fontWeight: FontWeight.bold);
+
+  // Historico
+  static TextStyle baseboard = GoogleFonts.inter(fontSize: 24, fontWeight: FontWeight.bold);
 }
