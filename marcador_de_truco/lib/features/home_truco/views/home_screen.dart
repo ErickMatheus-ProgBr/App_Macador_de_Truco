a8f3b11931e1bec66ebdee899b8f8c2d3a1ce756
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-07-06 22:59:22 -0300

    chore(style): dando vida ao app, cores, fontes , widgets

--- marcador_de_truco/lib/features/home_truco/views/home_screen.dart
+++ marcador_de_truco/lib/features/home_truco/views/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:marcador_de_truco/core/fonts/customFonts.dart';
+import 'package:marcador_de_truco/core/theme/theme_colors.dart';
 import 'package:marcador_de_truco/core/utils/media_query.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -12,13 +14,130 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: Row(
         children: [
-          Container(
-            margin: EdgeInsets.all(2),
-            width: CustomMediaquery.width(context),
-            height: CustomMediaquery.heightPorcentage(context, 40),
-            child: Column(),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: ThemeColors.myThemeApp.colorScheme.primary,
+              margin: EdgeInsets.zero,
+              width: CustomMediaquery.width(context),
+              height: CustomMediaquery.heightPorcentage(context, 100),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Time A",
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.titleLarge?.color,
+                        fontSize: Customfonts.timeNameStyle.fontSize,
+                      ),
+                    ),
+
+                    Text(
+                      "02",
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.bodyLarge?.color,
+                        fontSize: Customfonts.pointsStyle.fontSize,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(83, 219, 217, 217),
+                          ),
+                          child: Text(
+                            "-1", //
+                            style: TextStyle(
+                              color: TextTheme.of(context).bodyLarge?.color,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(83, 219, 217, 217),
+                          ),
+                          child: Text(
+                            "+1",
+                            style: TextStyle(
+                              color: TextTheme.of(context).bodyLarge?.color,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(
+              color: ThemeColors.myThemeApp.colorScheme.secondary,
+              margin: EdgeInsets.zero,
+              width: CustomMediaquery.width(context),
+              height: CustomMediaquery.heightPorcentage(context, 100),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Time B",
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.titleLarge?.color,
+                        fontSize: Customfonts.timeNameStyle.fontSize,
+                      ),
+                    ),
+                    Text(
+                      "07",
+                      style: TextStyle(
+                        color: Theme.of(context).textTheme.bodyLarge?.color,
+                        fontSize: Customfonts.pointsStyle.fontSize,
+                      ),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(83, 219, 217, 217),
+                          ),
+                          child: Text(
+                            "-1", //
+                            style: TextStyle(
+                              color: TextTheme.of(context).bodyLarge?.color,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color.fromARGB(83, 219, 217, 217),
+                          ),
+                          child: Text(
+                            "+1",
+                            style: TextStyle(
+                              color: TextTheme.of(context).bodyLarge?.color,
+                              fontSize: 18,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
