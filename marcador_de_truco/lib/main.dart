58af240017b59ce2cf95494b499d7c00446b4c0a
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-07-09 22:24:46 -0300

    feat: importando o 'ChangeNotifierProvider' dentro da nossa main

--- marcador_de_truco/lib/main.dart
+++ marcador_de_truco/lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:marcador_de_truco/core/theme/theme_colors.dart';
+import 'package:marcador_de_truco/features/home_truco/provider/truco_provider.dart';
 import 'package:marcador_de_truco/features/home_truco/views/home_screen.dart';
+import 'package:provider/provider.dart'; // 👈 ADICIONE ESSA LINHA AQUI!
+
+import 'package:path_provider/path_provider.dart';
 
 void main() {
-  runApp(AppTruco());
+  runApp(
+    MultiProvider(
+      providers: [
+        // 1. Envelopamos o app todo com o Provedor de Estado
+        ChangeNotifierProvider(
+          create: (context) => ScoreCounter(), // 👈 Instancia o seu controller aqui
+          child: const AppTruco(),
+        ),
+      ],
+    ),
+  );
 }
 
 class AppTruco extends StatelessWidget {
