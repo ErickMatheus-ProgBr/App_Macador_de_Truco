32a8bd96166d7ba1c61d772bd3e14ed05f4d4f39
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-07-09 22:50:05 -0300

    fix: integrando o TrucoModel dentro do ScorCounter

--- marcador_de_truco/lib/features/home_truco/provider/truco_provider.dart
+++ marcador_de_truco/lib/features/home_truco/provider/truco_provider.dart
@@ -1,30 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:marcador_de_truco/features/home_truco/data/models/truco_model.dart';
 
 class ScoreCounter extends ChangeNotifier {
-  // classe para gerenciar estado
-  int _points = 0; // criamos uma variavel para mostrar que os pontos começam com zero
+  // Inicializamos o nosso modelo com os valores padrão do jogo
+  TrucoModel _trucoData = TrucoModel(
+    gameName: "Truco",
+    caption: "Marcador",
+    timeA: "Time A",
+    timeB: "Time B",
+    pointsA: 0,
+    pointsB: 0,
+  );
 
-  int get points => _points; // _points é privado, o gettter "points" expções o valor atual;
+  // Getter para a HomeScreen conseguir ler todos os dados
+  TrucoModel get trucoData => _trucoData;
 
-  void increasePoints() {
-    // função para incrementar Pontos na Partida;
-    if (_points > 12) {
-      _points++;
-      notifyListeners();
+  // Função para incrementar pontos do Time A
+  void increasePointsA() {
+    if (_trucoData.pointsA < 12) {
+      // O jogo de truco vai até 12!
+      _trucoData = TrucoModel(
+        gameName: _trucoData.gameName,
+        caption: _trucoData.caption,
+        timeA: _trucoData.timeA,
+        timeB: _trucoData.timeB,
+        pointsA: _trucoData.pointsA + 1, // Incrementa o time A
+        pointsB: _trucoData.pointsB,
+      );
+      notifyListeners(); // Avisa a HomeScreen para atualizar
     }
   }
 
-  void decreaseStitches() {
-    // Função para decrementar Pontos na Partida;
-    if (_points < 0) {
-      _points--;
-      notifyListeners();
-    }
-  }
-
-  void reset() {
-    // Função para Reiniciar a Partida
-    _points = 0;
-    notifyListeners();
-  }
+  // Crie funções parecidas para diminuir e para gerenciar o Time B...
 }
