20db7e6ccf55884e54bb8149a08beca6dfb8f1dd
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-07-16 20:37:37 -0300

    chore: adicionando uma nova cor para classe

--- marcador_de_truco/lib/core/theme/theme_colors.dart
+++ marcador_de_truco/lib/core/theme/theme_colors.dart
@@ -3,7 +3,11 @@ import 'package:flutter/material.dart';
 class ThemeColors {
   // O static permite acessar direto como: ThemeColors.myThemeApp
   static ThemeData myThemeApp = ThemeData(
-    colorScheme: ColorScheme.fromSwatch().copyWith(primary: Colors.red, secondary: Colors.black),
+    colorScheme: ColorScheme.fromSwatch().copyWith(
+      primary: Colors.red,
+      secondary: Colors.black,
+      tertiary: Colors.white,
+    ),
     textTheme: const TextTheme(
       // Agrupamentos de estilo de texto para o app.
       bodyLarge: TextStyle(color: Colors.white), // estilo pré-definido
