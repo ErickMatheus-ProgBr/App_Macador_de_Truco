96c10edeee4020472b109f3a73119b142ae4e570
ErickMatheus-ProgBr <erickmatheus.prog@gmail.com>
2026-07-18 09:11:16 -0300

    feat(model): implementar suporte a vencedor no TrucoModel
    - Adicionados os campos winner e winnerTeam ao modelo
    - Implementado o metodo copyWith para facilitar a imutabilidade
    - Corrigido o mapeamento de tipos no metodo TrucoModel.fromJson

--- marcador_de_truco/lib/features/home_truco/data/models/truco_model.dart
+++ marcador_de_truco/lib/features/home_truco/data/models/truco_model.dart
@@ -8,6 +8,8 @@ class TrucoModel {
   final int pointsA;
   final int pointsB;
   final String btntruco;
+  final bool winner;
+  final String winnerTeam;
 
   TrucoModel({
     required this.gameName,
@@ -19,7 +21,39 @@ class TrucoModel {
     required this.pointsA,
     required this.pointsB,
     required this.btntruco,
+    required this.winner,
+    required this.winnerTeam,
   });
+
+  // 💡 A MÁGICA ESTÁ AQUI: Esse método clona o modelo alterando apenas os parâmetros enviados
+  // O método copyWith serve para "copiar" um objeto alterando apenas o que você pedir.
+  TrucoModel copyWith({
+    String? gameName,
+    String? caption,
+    String? timeA,
+    String? timeB,
+    String? decre,
+    String? incre,
+    int? pointsA,
+    int? pointsB,
+    String? btntruco,
+    bool? winner,
+    String? winnerTeam,
+  }) {
+    return TrucoModel(
+      gameName: gameName ?? this.gameName,
+      caption: caption ?? this.caption,
+      timeA: timeA ?? this.timeA,
+      timeB: timeB ?? this.timeB,
+      decre: decre ?? this.decre,
+      incre: incre ?? this.incre,
+      pointsA: pointsA ?? this.pointsA,
+      pointsB: pointsB ?? this.pointsB,
+      btntruco: btntruco ?? this.btntruco,
+      winner: winner ?? this.winner,
+      winnerTeam: winnerTeam ?? this.winnerTeam,
+    );
+  }
 
   factory TrucoModel.fromJson(Map<String, dynamic> json) {
     return TrucoModel(
@@ -32,6 +66,8 @@ class TrucoModel {
       pointsA: json["Pontos Time A"] ?? 0,
       pointsB: json["Pontos Time B"] ?? 0,
       btntruco: json["TRUCOOOO!"] ?? "TRUCOOOO!",
+      winner: json["winner"] ?? false,
+      winnerTeam: json["Time vencedor:"],
     );
   }
 }
